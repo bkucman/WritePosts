18a43fb3fa87634dd213c16a4cb59bbffc8c24c9
Blazej <kucmeno12@o2.pl>
2018-05-14 14:25:18 +0200

    add roels

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-   has_many :topics, dependent: :destroy
+  has_many :topics, dependent: :destroy
+  
+  def admin?
+  	has_role?(:admin)
+  end
+
+  def moderator?
+  	has_role?(:moderator)
+  end
+
+  def simple_user?
+  	has_role?(:simple_user)
+  end
 end
